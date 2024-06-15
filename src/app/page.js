8e08a48c7bd1fc09bb389b4f84e9506b891db819
melchanatan <chanatan@outlook.com>
2024-06-15 20:31:39 +0700

    feat: init repo with fonts and colors

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-between p-24 bg-background font-raster">
+      <h1 className="text-6xl font-bold text-primary">Home</h1>
+    </main>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+      colors: {
+        'background': {
+          DEFAULT: '#F6F2F2',
+          darken: '#F1E4E4',
+        },
+        'primary': {
+          DEFAULT: '#1D1E21',
+          lighten: '#2C2D32',
+        }
+      },
+      fontFamily: {
+        'raster': 'Raster Grotesk',
+        'tiny5': 'Tiny5'
+      }
+  },
+  plugins: [],
+};
