ea63335990bfc154c8fe80b2500ba4555180b6af
melchanatan <chanatan@outlook.com>
2024-06-16 22:08:09 +0700

    feat: import folder 3d model

--- src/components/Folder3d.js
+++ src/components/Folder3d.js
@@ -1,58 +1,82 @@
 "use client";
 import { createRoot } from "react-dom/client";
 import React, { useRef, useState } from "react";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { Html, useGLTF, SoftShadows, ScrollControls, useScroll, useTexture } from '@react-three/drei';
+import { Canvas, useFrame, useThree,  } from "@react-three/fiber";
+import {
+  Html,
+  useGLTF,
+  SoftShadows,
+  ScrollControls,
+  CameraControls,
+  useScroll,
+  useTexture,
+} from "@react-three/drei";
 
 function Box(props) {
   // This reference will give us direct access to the mesh
   const meshRef = useRef();
   const { viewport } = useThree();
-  const { nodes, materials } = useGLTF('/models/earth.gltf')
-
+  const { nodes } = useGLTF("/models/folder/model.gltf");
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
 
-  useFrame(({ mouse }) => {
-    const x = (mouse.x * viewport.width) / 2;
-    const y = (mouse.y * viewport.height) / 2;
-    meshRef.current.position.set(x, y, 0);
-    meshRef.current.rotation.set(-y, x, 0);
-  });
+  console.log(nodes);
+  // useFrame(({ mouse }) => {
+  //   const x = (mouse.x * viewport.width) / 2;
+  //   const y = (mouse.y * viewport.height) / 2;
+  //   meshRef.current.position.set(x, y, 0);
+  //   meshRef.current.rotation.set(-y, x, 0);
+  // });
 
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+  // useFrame((state, delta) => (meshRef.current.rotation.x += delta));
 
   return (
     <mesh
       {...props}
       ref={meshRef}
-      scale={active ? 1.5 : 1}
+      scale={0.1}
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
     >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-      {/* <mesh geometry={nodes["Area_(2)"].geometry} material={materials.aluminium} material-color="#aaaaaf" material-envMapIntensity={0.2} /> */}
-
+      <mesh
+        geometry={nodes.front.geometry}
+        position={[2, 0, 0]}
+      >
+        <meshStandardMaterial
+            color="#F4D44A"
+         />
+      </mesh>
+      
+      <mesh
+        geometry={nodes.middle.geometry}
+        position={[.3, 0, 0]}
+      >
+        <meshStandardMaterial
+         />
+      </mesh>
+      <mesh
+        geometry={nodes.back.geometry}
+        position={[-2.45, .98, 0]}
+      >
+        <meshStandardMaterial
+            color="#F4D44A"
+            />
+      </mesh>
     </mesh>
   );
 }
 
 const MyCanvas = () => {
+  const cameraControlRef = useRef(null);
+
   return (
     <Canvas>
       <ambientLight intensity={Math.PI / 2} />
-      <spotLight
-        position={[10, 10, 10]}
-        angle={0.15}
-        penumbra={1}
-        decay={0}
-        intensity={Math.PI}
-      />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <CameraControls ref={cameraControlRef} />
+
+      <Box position={[0, 0, 0]} />
     </Canvas>
   );
 };
