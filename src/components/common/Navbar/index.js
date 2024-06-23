addc1f8898a5226b8996ce9b2d08e695356ea41d
melchanatan <chanatan@outlook.com>
2024-06-23 20:51:17 +0700

    feat: add animation on navbar

--- src/components/common/Navbar/index.js
+++ src/components/common/Navbar/index.js
@@ -1,15 +1,35 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Logo from "./Logo";
+import { useSpring, animated } from "react-spring";
+
 const Navbar = () => {
+  const [startAnimation, setStartAnimation] = useState(false);
+
+  const spring = useSpring({
+    y: startAnimation ? 0 : -100,
+    from: { y: -100 },
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setStartAnimation(true);
+    }, 4800);
+  }, []);
+
   return (
-    <nav className="z-50 absolute px-page top-5 flex justify-between items-center w-full text-primary text-lg font-tiny5">
+    <animated.nav
+      style={spring}
+      className="z-50 absolute px-page top-5 flex justify-between items-center w-full text-primary text-lg font-tiny5"
+    >
       <Logo className="absolute left-page" />
       <div className="absolute left-1/2 -translate-x-1/2 flex gap-[6vw]">
         <a href="">team</a>
         <a href="">playground</a>
       </div>
       <button>EN/TH</button>
-    </nav>
+    </animated.nav>
   );
 };
 
